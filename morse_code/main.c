bd96aa4f3454dcda53335716932cb351fddf8d25
snowyiy <enek54363027@gmail.com>
2024-10-23 18:01:13 +0200

    now it works

--- morse_code/main.c
+++ morse_code/main.c
@@ -1,57 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>  // strlen()
+
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof(x[0]))
+
+struct Morseletter {char letter; char * code_letter;};
 
 
 int main() {
     // dictionary for translate letter to morse
-    char morse_letter[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
-                            'x', 'y', 'z'};
-    char morse_letter_code[][26] = {"12", "2111", "2121", "211", "1", "1121", "221", "1111", "11", "1222", "212", "1211", "22", "21", "222",
-                                    "1221", "2212", "121", "111", "2", "112", "1112", "122", "2112", "2122", "2211"};
+    struct Morseletter morse[26] = {{'a', "01"}, {'b', "1000"}, {'c', "1010"}, {'d', "100"}, {'e', "0"}, {'f', "0010"}, {'g', "110"},
+                                    {'h', "0000"}, {'i', "00"}, {'j', "0111"}, {'k', "101"}, {'l', "0100"}, {'m', "11"}, {'n', "10"},
+                                    {'o', "111"}, {'p', "0110"}, {'q', "1101"}, {'r', "010"}, {'s', "000"}, {'t', "1"}, {'u', "001"},
+                                    {'v', "001"}, {'w', "011"}, {'x', "1001"}, {'y', "1011"}, {'z', "1100"}};
 
 
     // take a message to translate from user
-    int buffer = 1024;
-    char message[1024];
-    printf("enter a message (1024 char max):\n");
-    scanf("%s", message);  // \0
+    int buffer = 256;
+    char * message = malloc(buffer * sizeof(char));
+    printf("enter a message :\n");
+    scanf("%s", message);
 
-    // for (int i = 0; i < size; i++) {
-    //     printf("%c", message[i]);
-    // }
+    // store the size of message into size
+    int size = strlen(message);
+    printf("size fo message : %d\n", size);
 
-    // count spaces at the end
-    int spaces = 0;
-    int i = buffer;
-    while (message[i] != EOF) {
-        spaces++;
-        i--;
-    }
-
-    // set size to the size of message without the spaces at the end
-    int size = buffer - spaces;
-    // printf("size : %d\n", size);
-    // printf("spaces : %d\n", spaces);
-
-    // set the size of the translated message
-    char *message_morse = malloc(size * sizeof(char*));
-
-    // !-DOESN'T WORK -> pointer
-    // code the message
+    // print the message
     for (int i = 0; i < size; i++) {
-        for (int j = 0; i < 27; i++) {
-            if (message[i] == morse_letter[j]) {     //!
-                message_morse[i] = morse_letter_code[j];  //!
-            }
+        printf("%c", message[i]);
+    }
+    printf("\n");
+
+    char ** message_morse = malloc(size * sizeof(char));
+
+    // translate message
+    for (int i = 0; i < size; i++) {
+        int j = 0;
+        while (message[i] != morse[j].letter && j < ARRAY_SIZE(morse) + 1) {
+            j++;
         }
+        message_morse[i] = morse[j].code_letter;
     }
 
     // print the coded message
     for (int i = 0; i < size; i++) {
-        printf("%c", message_morse[i]);
+        printf("%s ", message_morse[i]);
     }
     printf("\n");
 
+    int k = 7;
+    printf("%c", k);
+
+    free(message);
     free(message_morse);
 
     return 0;
