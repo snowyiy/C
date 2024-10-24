8d8973c9b9c8f163030ae62fa205faf434d4cae3
snowyiy <enek54363027@gmail.com>
2024-10-24 15:52:30 +0200

    doesn't work anymore, try to make a decode function and modify the translate function

--- morse_code/main.c
+++ morse_code/main.c
@@ -3,27 +3,30 @@
 #include <string.h>  // strlen()
 
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof(x[0]))
+#define BUFFER 1024
+
+typedef enum {False, True} bool;
 
 struct Morseletter {char letter; char * code_letter;};
-
 // dictionary for translate letter to morse
-struct Morseletter morse[26] = {{'a', "01"}, {'b', "1000"}, {'c', "1010"}, {'d', "100"}, {'e', "0"}, {'f', "0010"}, {'g', "110"},
+struct Morseletter morse[27] = {{'a', "01"}, {'b', "1000"}, {'c', "1010"}, {'d', "100"}, {'e', "0"}, {'f', "0010"}, {'g', "110"},
                                 {'h', "0000"}, {'i', "00"}, {'j', "0111"}, {'k', "101"}, {'l', "0100"}, {'m', "11"}, {'n', "10"},
                                 {'o', "111"}, {'p', "0110"}, {'q', "1101"}, {'r', "010"}, {'s', "000"}, {'t', "1"}, {'u', "001"},
-                                {'v', "001"}, {'w', "011"}, {'x', "1001"}, {'y', "1011"}, {'z', "1100"}};
+                                {'v', "001"}, {'w', "011"}, {'x', "1001"}, {'y', "1011"}, {'z', "1100"}, {' ', " "}};
 
 
 // take a message to translate from user
-void take_message(char * message) {
+void takeMessage(char * message) {
     printf("enter a message :\n");
     scanf("%s", message);
 
 }
 
 // translate message
-void translate(char * t_message, char ** t_message_morse, int t_size) {//, struct Morseletter * t_morse, int t_array_size) {
+void translateToMorse(char * t_message, char ** t_message_morse, int t_size) {
     for (int i = 0; i < t_size; i++) {
         int j = 0;
+        char * str_buffer;
         while (t_message[i] != morse[j].letter && j < ARRAY_SIZE(morse) + 1)
             j++;
         t_message_morse[i] = morse[j].code_letter;
@@ -31,12 +34,16 @@ void translate(char * t_message, char ** t_message_morse, int t_size) {//, struc
         // display the coded message
         int code_letter_size = strlen(morse[j].code_letter);
         for (int h = 0; h < code_letter_size; h++) {
-            if (morse[j].code_letter[h] == '1')
+            if (morse[j].code_letter[h] == '0') {
                 // dot
-                printf("- ");
-            else
+                printf(". ");
+            } else if (morse[j].code_letter[h] == '1') {
                 // dash
-                printf("--- ");
+                printf("- ");
+            } else {
+                // spaces
+                printf("       ");
+            }
         }
         // space beetween letters 
         printf("   ");
@@ -45,11 +52,10 @@ void translate(char * t_message, char ** t_message_morse, int t_size) {//, struc
 }
 
 
-void code() {
+char ** code() {
 
-    int buffer = 256;
-    char * message = malloc(buffer * sizeof(char));
-    take_message(message);
+    char * message = malloc(BUFFER * sizeof(char));
+    takeMessage(message);
 
     // store the size of message into size
     int size = strlen(message);
@@ -57,7 +63,7 @@ void code() {
     // define coded message
     char ** message_morse = malloc(size * sizeof(char));
 
-    translate(message, message_morse, size);
+    translateToMorse(message, message_morse, size);
 
     // print the coded message
     for (int i = 0; i < size; i++)
@@ -66,12 +72,15 @@ void code() {
 
     free(message);
     free(message_morse);
-
 }
 
 
 void decode() {
+    char * message_decode = malloc(BUFFER * sizeof(char));
 
+
+
+    free(message_decode);
 }
 
 
@@ -80,7 +89,7 @@ int main() {
     code();
 
     // decode a message
-    //decode()
+    decode();
 
     return 0;
 }
