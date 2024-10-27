496742066f5c62ed2d7e1793c6759aa1a051b327
snowyiy <enek54363027@gmail.com>
2024-10-27 12:14:04 +0100

    made powi a "library" (clean)

--- convert/main.c
+++ convert/main.c
@@ -5,15 +5,6 @@
 #include "powi.h"
 
 
-// this function returns a to the power of b
-//int powi(int a, int b) {
-//    int r = 1;
-//    for (int i = 0; i < b; i++) {
-//        r *= a;
-//    }
-//    return r;
-//}
-
 
 // convert binary into decimal
 int decimal(int binary) {
