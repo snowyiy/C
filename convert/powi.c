2c3a48ccc7337f5b2290aa3c2c885d345782f4c6
snowyiy <enek54363027@gmail.com>
2024-10-27 12:13:30 +0100

    made powi a "library"

--- convert/powi.c
+++ convert/powi.c
@@ -1,7 +1,10 @@
+#include "powi.h"
+
 // this function returns a to the power of b
 int powi(int a, int b) {
     int r = 1;
     for (int i = 0; i < b; i++)
         r *= a;
+    return r;
 }
     
--- convert/powi.h
+++ convert/powi.h
@@ -1,1 +1,6 @@
-int powi(int a, int b)
+#ifndef POWI_H_
+#define POWI_H_
+
+int powi(int a, int b);
+
+#endif
