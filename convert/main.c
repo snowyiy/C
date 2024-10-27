8313ff47544dbff853224987127df61b232274eb
enekraftii <144264800+snowyiy@users.noreply.github.com>
2024-10-27 02:37:08 +0100

    Update main.c

--- convert/main.c
+++ convert/main.c
@@ -2,15 +2,17 @@
 #include <stdlib.h>
 #include <math.h>
 
+#include "powi.h"
+
 
 // this function returns a to the power of b
-int powi(int a, int b) {
-    int r = 1;
-    for (int i = 0; i < b; i++) {
-        r *= a;
-    }
-    return r;
-}
+//int powi(int a, int b) {
+//    int r = 1;
+//    for (int i = 0; i < b; i++) {
+//        r *= a;
+//    }
+//    return r;
+//}
 
 
 // convert binary into decimal
